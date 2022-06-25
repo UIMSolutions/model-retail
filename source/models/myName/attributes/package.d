149f1e2e0a9c4b4e2b15c40bdc5df3d1cd820dc1
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-25 17:49:52 +0200

    Update and rename source/models/myName/attributes/package.d to source/models/retail/attributes/package.d

--- source/models/myName/attributes/package.d
+++
@@ -1,7 +0,0 @@
-module models.myName.attributes;
-
-public import models.myName.attributes.codes;
-public import models.myName.attributes.elements;
-public import models.myName.attributes.entites;
-public import models.myName.attributes.functions;
-public import models.myName.attributes.uuids;

f8c0b777b2fe27816f255b348b2aafe22a6dade5
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-25 17:47:45 +0200

    Initial commit

---
+++ source/models/myName/attributes/package.d
@@ -0,0 +1,7 @@
+module models.myName.attributes;
+
+public import models.myName.attributes.codes;
+public import models.myName.attributes.elements;
+public import models.myName.attributes.entites;
+public import models.myName.attributes.functions;
+public import models.myName.attributes.uuids;
